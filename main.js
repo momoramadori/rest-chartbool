abb501963c907bf0de5aed9719869faf154ed520
Momo Ramadori <momoramadori8@gmail.com>
2020-06-10 17:44:05 +0200

    Milestone 1 done!

--- main.js
+++ main.js
@@ -32,22 +32,19 @@ $(document).ready(function(){
             'November': 0,
             'December': 0
         };
+
         for (let index = 0; index < data.length; index++) {
             var singoloEvento = data[index];
             var singolaVendita = singoloEvento.amount;
             var formatoData = moment(data[index].date, "DD/MM/YYYY");
             var meseVendita = formatoData.format("MMMM");
-            console.log(meseVendita);
-            
             if (venditePerMese.hasOwnProperty(meseVendita)) {
                 venditePerMese[meseVendita] += singolaVendita
             }
         }
-        console.log(venditePerMese);
         var mesi = Object.keys(venditePerMese);
         var vendite = Object.values(venditePerMese);
-        generaBar(mesi,vendite);
-       
+        generaBar(mesi,vendite); 
     }
 
     function ottieniVenditeVenditore(data) {
@@ -63,131 +60,129 @@ $(document).ready(function(){
                 venditePerVenditore[nomeVenditore] += singolaVendita
             }
             totaleVendite += singolaVendita;
-            
         }
-        console.log(totaleVendite);
+
         for (const nomeVenditore in venditePerVenditore) {
             var element = venditePerVenditore[nomeVenditore]
             element = ((venditePerVenditore[nomeVenditore] / totaleVendite)) * 100;
             venditePerVenditore[nomeVenditore] = element.toString().slice(0,5);
-            
-        }        
+        }     
+
         var nomi = Object.keys(venditePerVenditore);
         var vendite = Object.values(venditePerVenditore);
         generaPie(nomi,vendite);
-       
     }
 
     function generaBar(mesi,vendite) {
         var ctx = $('#myChart_bar')[0].getContext('2d');
-            var myChart = new Chart(ctx, {
-                type: 'bar',
-                data: {
-                    labels: mesi,
-                    datasets: [{
-                        label: 'Total Sales',
-                        data: vendite,
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)',
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: mesi,
+                datasets: [{
+                    label: 'Total Sales',
+                    data: vendite,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)',
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
 
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)',
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
-                        borderWidth: 1
-                    }]
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)',
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                title: {
+                    display: true,
+                    text: 'Monthly amount of sales'
                 },
-                options: {
-                    title: {
-                        display: true,
-                        text: 'Monthly amount of sales'
-                    },
-                    legend: {
-                        display: false,
-                    },
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true
-                            }
-                        }]
-                    }
+                legend: {
+                    display: false,
+                },
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
                 }
-            })
+            }
+        })
     }
 
     function generaPie(nomi,vendite) {
         var ctx = $('#myChart_pie')[0].getContext('2d');
-            var myChart = new Chart(ctx, {
-                type: 'pie',
-                data: {
-                    labels: nomi,
-                    datasets: [{
-                        label: 'Total Sales',
-                        data: vendite,
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)',
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
+        var myChart = new Chart(ctx, {
+            type: 'pie',
+            data: {
+                labels: nomi,
+                datasets: [{
+                    label: 'Total Sales',
+                    data: vendite,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)',
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
 
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)',
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
-                        borderWidth: 1
-                    }]
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)',
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                title: {
+                    display: true,
+                    text: 'Salesman\'s Sales'
                 },
-                options: {
-                    title: {
-                        display: true,
-                        text: 'Salesman\'s Sales'
-                    },
-                    legend: {
-                        display: false,
-                    },
-                }
-            })
+                legend: {
+                    display: false,
+                },
+            }
+        })
     }
 })
