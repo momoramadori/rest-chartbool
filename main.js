3e3e88d98165e538f800ad52724ab72db60b1de5
Momo Ramadori <momoramadori8@gmail.com>
2020-06-11 09:57:48 +0200

    added lineTensio to Line graph

--- main.js
+++ main.js
@@ -82,21 +82,6 @@ $(document).ready(function(){
                 datasets: [{
                     label: 'Total Sales',
                     data: vendite,
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)',
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-
-                    ],
                     borderColor: [
                         'rgba(255, 99, 132, 1)',
                         'rgba(54, 162, 235, 1)',
@@ -111,8 +96,9 @@ $(document).ready(function(){
                         'rgba(153, 102, 255, 1)',
                         'rgba(255, 159, 64, 1)'
                     ],
-                    borderWidth: 1
-                }]
+                    borderWidth: 1,
+                    lineTension: 0
+                }],
             },
             options: {
                 title: {
